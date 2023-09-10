ec5462f5ba0e6106cd34c64ba3805128c5401d34
João Matias <joao.matias@talkdesk.com>
2023-09-10 19:48:01 +0100

    small change in Spell algo

--- logparser/Spell/Spell.py
+++ logparser/Spell/Spell.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # =========================================================================
 
-import regex as re
+import re
 import os
 import pandas as pd
 import hashlib
@@ -343,7 +343,7 @@ class LogParser:
         regex = ""
         for k in range(len(splitters)):
             if k % 2 == 0:
-                splitter = re.sub(" +", "\s+", splitters[k])
+                splitter = re.sub(' +', '\\\s+', splitters[k])
                 regex += splitter
             else:
                 header = splitters[k].strip("<").strip(">")
@@ -353,12 +353,12 @@ class LogParser:
         return headers, regex
 
     def get_parameter_list(self, row):
-        template_regex = re.sub(r"<.{1,5}>", "<*>", row["EventTemplate"])
+        template_regex = re.sub(r"\s<.{1,5}>\s", "<*>", row["EventTemplate"])
         if "<*>" not in template_regex:
             return []
         template_regex = re.sub(r"([^A-Za-z0-9])", r"\\\1", template_regex)
-        template_regex = re.sub(r"\\ +", r"\\s+", template_regex)
-        template_regex = "^" + template_regex.replace("\<\*\>", "(.*?)") + "$"
+        template_regex = re.sub(r'\\ +', r'[^A-Za-z0-9]+', template_regex)
+        template_regex = "^" + re.sub(r"(\\\<\\\*\\\>)+", "(.*?)", template_regex) + "$"
         parameter_list = re.findall(template_regex, row["Content"])
         parameter_list = parameter_list[0] if parameter_list else ()
         parameter_list = (
